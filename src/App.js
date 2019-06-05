6327da1d04cd09ee28f79807a4b312e6c2869eb9
NirobNabil <thetwin11100@gmail.com>
2019-06-05 09:13:29 +0600

    created the skeleton(just the folder structure)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Swipeable } from 'react-touch'
+
+function showSwipe(){
+  console.log("lorem got swiped")
+  let synth = window.speechSynthesis;
+  let voices = synth.getVoices()
+  let text = new SpeechSynthesisUtterance("Swiped left")
+  synth.speak(text);
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Swipeable onSwipeLeft={showSwipe}>
+        <div className="swipe"> lorem ipsum doler motor </div>
+      </Swipeable>
     </div>
   );
 }
